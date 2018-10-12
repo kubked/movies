0dcf90af1ba5445e27b525f523d7e5fd0fd3adc3
kubked <kubked@gmail.com>
2018-10-12 11:16:46 +0200

    update travis secure env

--- src/api/views.py
+++ src/api/views.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 class MovieList(mixins.ListModelMixin,
+                mixins.RetrieveModelMixin,
                 viewsets.GenericViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
